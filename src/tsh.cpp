dbc10c2d0c99bdf5b404f092f649b16454c963c3
Anthony Boccadoro <aboccadoro@umass.edu>
2017-02-14 05:28:42 -0500

    exec_command

---
+++ src/tsh.cpp
@@ -0,0 +1,22 @@
+#include "tsh.h"
+
+using namespace std;
+
+void simple_shell::parse_command(char* cmd, char** cmdTokens) {
+	*cmdTokens = strtok(cmd, " \n");
+	while(*cmdTokens != NULL) {
+		cmdTokens++;
+		*cmdTokens = strtok(NULL, " \n");
+	}
+}
+
+void simple_shell::exec_command(char **argv) {
+  // TODO: fork a child process to execute the command.
+  // parent process should wait for the child process to complete and reap it
+
+}
+
+bool simple_shell::isQuit(char *cmd) {
+	if(strcmp(cmd, "quit") == 0) return true;
+  	return false;
+}
