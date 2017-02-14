6c4a12efc920374434c895ccd3f7d05cddb355a6
Anthony Boccadoro <aboccadoro@umass.edu>
2017-02-14 12:13:22 -0500

    class work

--- src/tsh.cpp
+++ src/tsh.cpp
@@ -13,7 +13,13 @@ void simple_shell::parse_command(char* cmd, char** cmdTokens) {
 void simple_shell::exec_command(char **argv) {
   // TODO: fork a child process to execute the command.
   // parent process should wait for the child process to complete and reap it
-
+	pid_t pid = fork();
+	if(pid == 0) {
+		System();
+	}
+	else if(pid > 0) {
+		waitpid(pid);
+	}
 }
 
 bool simple_shell::isQuit(char *cmd) {
